8603e3678368616a3d5f4c0f82edb55c37a37353
Creator-PA-1 <saip61@outlook.com>
2025-10-18 17:28:05 +0530

    sidebar-updates

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,18 @@
 import { useEffect, useState } from "react";
 import "./Sidebar.css";
 import logo from "../assets/logo.png";
+import removeBgIcon from "../assets/remove-bg-icon.png";
+import removeBgNavIcon from "../assets/remove-bg-nav-icon.png";
+import bulkBgRemoveIcon from "../assets/bulk-bg-remove-icon.png";
+import videoBgRemoverIcon from "../assets/video-bg-remover-icon.png";
+import pricingIcon from "../assets/pricing-icon.png";
+import creditsIcon from "../assets/credits-icon.png";
+import inviteFriendsImg from "../assets/invite-friends-img.png";
+import userProfileIcon from "../assets/user-profile-icon.png";
+import settingsIcon from "../assets/settings-icon.png";
+import sidebarCollapseIcon from "../assets/sidebar-collapse-icon.png";
 import { NavLink } from "react-router-dom";
+import { BiColor } from "react-icons/bi";
 
 const Sidebar = (props) => {
   const [collapsed, setCollapsed] = useState(false);
@@ -35,18 +46,39 @@ const Sidebar = (props) => {
       }
     >
       <div className={collapsed ? "logo-section-collapsed" : "logo-section"}>
-        <img id={collapsed ? "logo-collapsed" : "logo"} src={logo} />
-        <p id={collapsed ? "app-title-collapsed" : "app-title"}>BG Remover</p>
-        {collapsed == false && (
-          <p id="collapse-icon" onClick={toggleSidebar}>
-            &#60;
+        <img
+          id={collapsed ? "app-logo-collapsed" : "app-logo"}
+          src={removeBgIcon}
+        />
+        <div className="logo-title-container">
+          <p className={collapsed ? "app-title-collapsed" : "app-title"}>
+            removebg
           </p>
+          <p
+            className={
+              collapsed
+                ? "app-title-collapsed"
+                : "app-title app-title-extension"
+            }
+          >
+            .world
+          </p>
+        </div>
+
+        {collapsed == false && (
+          <img
+            src={sidebarCollapseIcon}
+            id="collapse-icon"
+            onClick={toggleSidebar}
+          />
         )}
       </div>
       {collapsed == true && (
-        <p id="expand-icon" onClick={toggleSidebar}>
-          &#62;
-        </p>
+        <img
+          src={sidebarCollapseIcon}
+          id="expand-icon"
+          onClick={toggleSidebar}
+        />
       )}
       <div className={collapsed ? "nav-item-collapsed" : "nav-item"}>
         <NavLink
@@ -56,8 +88,11 @@ const Sidebar = (props) => {
             justifyContent: collapsed ? "center" : "flex-start",
           }}
         >
-          <img id={collapsed ? "logo-collapsed" : "logo"} src={logo} />
-          {!collapsed && <span>Explore</span>}
+          <img
+            id={collapsed ? "logo-collapsed" : "logo"}
+            src={removeBgNavIcon}
+          />
+          {!collapsed && <span>Remove BG</span>}
         </NavLink>
       </div>
       <div className={collapsed ? "nav-item-collapsed" : "nav-item"}>
@@ -68,8 +103,11 @@ const Sidebar = (props) => {
             justifyContent: collapsed ? "center" : "flex-start",
           }}
         >
-          <img id={collapsed ? "logo-collapsed" : "logo"} src={logo} />
-          {!collapsed && <span>Settings</span>}
+          <img
+            id={collapsed ? "logo-collapsed" : "logo"}
+            src={bulkBgRemoveIcon}
+          />
+          {!collapsed && <span>Bulk BG Remove</span>}
         </NavLink>
       </div>
       <div className={collapsed ? "nav-item-collapsed" : "nav-item"}>
@@ -80,9 +118,50 @@ const Sidebar = (props) => {
             justifyContent: collapsed ? "center" : "flex-start",
           }}
         >
-          <img id={collapsed ? "logo-collapsed" : "logo"} src={logo} />
-          {!collapsed && <span>Profile</span>}
+          <img
+            id={collapsed ? "logo-collapsed" : "logo"}
+            src={videoBgRemoverIcon}
+          />
+          {!collapsed && <span>Video BG Remover</span>}
         </NavLink>
+      </div>
+      <hr className="divider" />
+      {!collapsed && <p className="sidebar-headings">SUBSCRIPTION</p>}
+      <div className={collapsed ? "nav-item-collapsed" : "nav-item"}>
+        <NavLink
+          to="/profile"
+          className="nav-link"
+          style={{
+            justifyContent: collapsed ? "center" : "flex-start",
+          }}
+        >
+          <img id={collapsed ? "logo-collapsed" : "logo"} src={pricingIcon} />
+          {!collapsed && <span>Pricing</span>}
+        </NavLink>
+      </div>
+      <div className={collapsed ? "nav-item-collapsed" : "nav-item"}>
+        <NavLink
+          to="/profile"
+          className="nav-link"
+          style={{
+            justifyContent: collapsed ? "center" : "flex-start",
+          }}
+        >
+          <img id={collapsed ? "logo-collapsed" : "logo"} src={creditsIcon} />
+          {!collapsed && <span>Credits</span>}
+        </NavLink>
+      </div>
+
+      <div className="sidebar-footer">
+        {!collapsed && <img id="invite-friends-img" src={inviteFriendsImg} />}
+        <div className="user-profile-section">
+          {!collapsed && <img id="user-profile-icon" src={userProfileIcon} />}
+          {!collapsed && <p>Naveen Kumar</p>}
+          <img
+            id={collapsed ? "settings-icon-collapsed" : "settings-icon"}
+            src={settingsIcon}
+          />
+        </div>
       </div>
     </div>
   );
